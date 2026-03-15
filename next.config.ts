d7d17bdc3b1bd9dea9d4513c840c29d4ba95cde1
Mohammad Anees <mohammadanees@Mohammads-MacBook-Air.local>
2026-03-15 18:42:10 -0500

    redirect home to query page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  redirects: async () => [
+    {
+      source: '/',
+      destination: '/experience/query',
+      permanent: false,
+    }
+  ]
+
 };
 
 export default nextConfig;
